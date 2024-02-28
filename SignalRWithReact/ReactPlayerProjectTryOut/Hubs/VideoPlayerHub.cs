39a1978e3c26267aabc67a88ebc67cfe4e0bce10
Umutcan Elçi <umutcanelci@yaani.com>
2024-02-28 09:05:19 +0300

    React TryOut

---
+++ SignalRWithReact/ReactPlayerProjectTryOut/Hubs/VideoPlayerHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ReactPlayerProjectTryOut.Hubs;
+
+public class VideoPlayerHub : Hub
+{
+    public async Task SendPlayStatus(bool isPlaying)
+    {
+        await Clients.All.SendAsync("ReceivePlayStatus",isPlaying);
+    }
+}
