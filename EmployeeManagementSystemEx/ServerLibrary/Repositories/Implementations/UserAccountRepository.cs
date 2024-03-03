4a84b7838bcae9641be0f678351773f876024359
Umutcan Elçi <umutcanelci@yaani.com>
2024-03-03 18:21:57 +0300

    JWT config

---
+++ EmployeeManagementSystemEx/ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementations;
+
+public class UserAccountRepository : IUserAccount
+{
+    public Task<GeneralResponse> CreateAsync(Register user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LoginResponse> SignInAsync(Login user)
+    {
+        throw new NotImplementedException();
+    }
+}
