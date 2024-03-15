727f3854ca8c80e106bd5406338bc9d1890febc6
Umutcan Elçi <umutcanelci@yaani.com>
2024-03-15 16:15:55 +0300

    Adding Fiance app api and Make sure it is working

---
+++ FianceAppTutorial/frontend/src/api.tsx
@@ -0,0 +1,27 @@
+import axios, { AxiosResponse, AxiosError } from "axios";
+
+interface Company {
+  
+}
+
+interface SearchResponse {
+  data: Company[];
+}
+
+export const searchCompanies = async (query: string): Promise<SearchResponse | string> => {
+  try {
+    const response: AxiosResponse<SearchResponse> = await axios.get(
+      `https://financialmodelingprep.com/api/v3/search-ticker?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    ); 
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    if (axiosError.isAxiosError){
+      console.log("error message: ",axiosError.message);
+      return axiosError.message;
+    } else {
+      console.log("unexpected error: ",error);
+      return "An expected error has occurred";
+    }
+  }
+}
