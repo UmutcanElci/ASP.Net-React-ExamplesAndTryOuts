10788323c020f58e3dac6483ffa01f8df8e9634b
Umutcan Elçi <umutcanelci@yaani.com>
2024-03-18 17:25:03 +0300

    MAUI project setup

---
+++ MAUI/MAUIFirstTry/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUIFirstTry;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MAUI/MAUIFirstTry/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIFirstTry;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
