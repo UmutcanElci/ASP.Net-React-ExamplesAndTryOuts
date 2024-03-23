bb34415827802bfc0009533912ac995b31a7c061
Umutcan Elçi <umutcanelci@yaani.com>
2024-03-23 14:10:42 +0300

    Events Handling of List View

--- MAUI/Contacts.Maui/Views/ContactsPage.xaml.cs
+++ MAUI/Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -29,5 +29,17 @@ public partial class ContactsPage : ContentPage
         public string Name { get; set; }
         public string Email { get; set; }
     }
-    
+
+    private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+		if(listContacts.SelectedItem != null)
+		{
+			await Shell.Current.GoToAsync(nameof(EditContactPage));
+		}
+    }
+
+    private void listContacts_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        listContacts.SelectedItem = null;
+    }
 }
--- MAUI/Contacts.Maui/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Contacts.Maui.AssemblyInfo.cs
+++ MAUI/Contacts.Maui/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Contacts.Maui.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contacts.Maui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+efbb8b56114534261adc86a8165337d92be909ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f74537d369d894a930e468dc8795ef740a102040")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contacts.Maui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contacts.Maui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
