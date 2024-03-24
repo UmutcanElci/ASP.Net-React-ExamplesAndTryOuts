22807e5729e9046e7e15f25860dddbc752894755
Umutcan Elçi <umutcanelci@yaani.com>
2024-03-24 17:49:42 +0300

    Working in EditPage not inclide the ContactId yet

--- MAUI/Contacts.Maui/Models/ContactRepository.cs
+++ MAUI/Contacts.Maui/Models/ContactRepository.cs
@@ -10,15 +10,15 @@ namespace Contacts.Maui.Models
     {
         public static List<Contact> _contacts = new List<Contact>()
         {
-            new Contact { Name="John Doe", Email="JohnDoe@mail.com"},
-            new Contact { Name="Jane Doe", Email="JaneDoe@mail.com"},
-            new Contact { Name="Tom Hanks", Email="TomHanks@mail.com"},
-            new Contact { Name="Frank Liu", Email="FrankLiu@mail.com"},
+            new Contact { ContactId = 1,Name="John Doe", Email="JohnDoe@mail.com"},
+            new Contact { ContactId = 2,Name="Jane Doe", Email="JaneDoe@mail.com"},
+            new Contact { ContactId = 3,Name="Tom Hanks", Email="TomHanks@mail.com"},
+            new Contact { ContactId = 4,Name="Frank Liu", Email="FrankLiu@mail.com"},
         };
 
         public static List<Contact> GetContacts() => _contacts;
 
-        public static Contact GetContact(int contactId)
+        public static Contact GetContactById(int contactId)
         {
           return _contacts.FirstOrDefault(x => x.ContactId == contactId);
         }
--- MAUI/Contacts.Maui/Views/EditContactPage.xaml.cs
+++ MAUI/Contacts.Maui/Views/EditContactPage.xaml.cs
@@ -1,7 +1,11 @@
+using Contacts.Maui.Models;
+using Contact = Contacts.Maui.Models.Contact;
 namespace Contacts.Maui.Views;
 
+[QueryProperty(nameof(ContactId),"Id")]
 public partial class EditContactPage : ContentPage
 {
+	private Contact contact;
 	public EditContactPage()
 	{
 		InitializeComponent();
@@ -11,4 +15,13 @@ public partial class EditContactPage : ContentPage
     {
 		Shell.Current.GoToAsync("..");
     }
+
+	public string ContactId
+	{
+		set
+		{
+			contact = ContactRepository.GetContactById(int.Parse(value));
+			lblName.Text = contact.Name;
+		}
+	}
 }
