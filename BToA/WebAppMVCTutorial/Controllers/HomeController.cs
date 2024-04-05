1a664bf047d39fc24bcdf98c420405ba4b64a41d
Umutcan Elçi <umutcanelci@yaani.com>
2024-04-06 01:06:38 +0300

    Adding control and views need fix

--- BToA/WebAppMVCTutorial/Controllers/HomeController.cs
+++ BToA/WebAppMVCTutorial/Controllers/HomeController.cs
@@ -1,31 +1,21 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using WebAppMVCTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using WebAppMVCTutorial.Data;
+
 
 namespace WebAppMVCTutorial.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly MenuContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(MenuContext context)
     {
-        _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(await _context.Dishes.ToListAsync());
     }
 }
---
+++ BToA/WebAppMVCTutorial/Views/Menu/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppMVCTutorial.Views.Menu;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
